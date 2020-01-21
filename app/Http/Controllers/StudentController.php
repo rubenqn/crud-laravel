9b0082f4e1f794a07e9973a8cf944a33eaeb715b
maestro astilla <laratadetucasa@gmail.com>
2020-01-21 12:10:30 +0100

    iconos editar y borrar

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 class StudentController extends Controller
 {
     public function index(){
-        return view('welcome');
+        $estudiantes = Students::all();
+        return view('welcome',compact('estudiantes'));
     }
 
     public function create(){
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,37 @@
 
 
 @if (session('successMsg'))
-    <div class="alert alert-success" role="alert">
-        {{session('successMsg')}}
-    </div>
+<div class="alert alert-success" role="alert">
+    {{session('successMsg')}}
+</div>
 @endif
+
+<table class="table">
+    <thead class="black white-text">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Apellidos</th>
+            <th scope="col">E-mail</th>
+            <th scope="col">Teléfono</th>
+            <th scope="col">Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($estudiantes as $estudiante)
+        <tr>
+            <th scope="row">{{$estudiante->id}}</th>
+            <td>{{$estudiante->nombre}}</td>
+            <td>{{$estudiante->apellidos}}</td>
+            <td>{{$estudiante->mail}}</td>
+            <td>{{$estudiante->telefono}}</td>
+            <td>
+                <a class="btn btn-raised btn-primary btn-sm" href=""><i class="fas fa-user-edit"></i></a>
+                <a class="btn btn-raised btn-danger btn-sm" href=""><i class="fas fa-user-times"></i></a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 
 @endsection
